25a1315b450d83f0545617b086965b719d2efc1f
elliot-cx <derfire22@gmail.com>
2025-11-26 13:58:36 +0100

    fix D2/server path traversal

--- back/api/uploads/uploads_api.py
+++ back/api/uploads/uploads_api.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, request, send_file
+from werkzeug.utils import secure_filename
 
 uploads_api = Blueprint('uploads', __name__)
 
 @uploads_api.route('/')
 def get_upload():
-   return send_file('uploads/' + request.args.get("name"))
- 
+   filename = secure_filename(request.args.get("name"))
+   return send_file('uploads/' + filename) 
